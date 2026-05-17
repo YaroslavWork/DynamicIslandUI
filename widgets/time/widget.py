d17c03f92bbfac2ae6a49c56a77835eebe975ad3
YaroslavWork <zahorodnyi.yaroslav.work@gmail.com>
2026-05-18 01:32:50 +0200

    feat: migrate TimeWidget UI to Gtk.Template

--- widgets/time/widget.py
+++ widgets/time/widget.py
@@ -3,18 +3,21 @@ import time
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk, GLib
 
+import os
+
+@Gtk.Template(filename=os.path.join(os.path.dirname(__file__), 'builder.xml'))
 class Widget(Gtk.Box):
+    __gtype_name__ = 'TimeWidget'
+
+    time_label = Gtk.Template.Child()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.add_css_class("time-widget")
-        self.add_css_class("widget")
-        self.label = Gtk.Label()
-        self.append(self.label)
         self.update_time()
         # Update time every second
         GLib.timeout_add_seconds(1, self.update_time)
 
     def update_time(self):
         current_time = time.strftime("%H:%M:%S")
-        self.label.set_label(current_time)
+        self.time_label.set_label(current_time)
         return True # Return True to keep the timeout active
